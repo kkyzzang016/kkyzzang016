c63e0c19af8ab14dec92130bae8fcca914300e93
kkyzzang016 <kkyzzang016@nate.com>
2017-07-17 14:46:24 +0900

    2017-07-17

---
+++ JavaEx/src/exception/ThreadNameEx.java
@@ -0,0 +1,36 @@
+package exception;
+
+public class ThreadNameEx {
+
+	public static void main(String[] args) {
+		Thread maint = Thread.currentThread();
+		System.out.println("프로그램 시작 스레드 이름 : "+maint.getName());
+		
+		ThreadA ta = new ThreadA();
+		System.out.println("작업 스레드 이름 : "+ta.getName());
+		ta.start();
+		
+		ThreadB tb = new ThreadB();
+		System.out.println("작업 스레드 이름 : "+tb.getName());
+		tb.start();
+	}
+}
+class ThreadA extends Thread{
+	public ThreadA() {
+		setName("ThreadA");
+		
+	}
+	public void run() {
+		for(int i=0;i<2;i++) {
+			System.out.println(getName() + "가 출력한 내용");
+		}
+	}
+}
+class ThreadB extends Thread{
+	@Override
+	public void run() {
+		for(int i=0; i<2;i++) {
+			System.out.println(getName()+"가 출력한 내용");
+		}
+	}
+}
---
+++ JavaEx/src/exception/ThreadRectDraw.java
@@ -0,0 +1,81 @@
+package exception;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Frame;
+import java.awt.Graphics;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.util.Random;
+
+public class ThreadRectDraw extends Frame implements Runnable {
+   int x = 0;
+   int y = 20;
+   boolean xOrient, yOrient;
+
+   public ThreadRectDraw(String str) {
+      super(str);
+      setSize(300, 200);
+      setVisible(true);
+      addWindowListener(new WindowAdapter() {
+         public void windowClosing(WindowEvent e) {
+            System.exit(0);
+         }
+      });
+   }
+
+   public void paint(Graphics gr) {
+      Random ran = new Random();
+      Dimension d = this.getSize();
+      if (xOrient) {
+         x--;
+         if (x < 0) {
+            x = 0;
+            xOrient = false;
+         }
+      } else {
+         x++;
+         if (x >= d.width - 20) {
+            x = d.width - 20;
+            xOrient = true;
+         }
+      }
+      if (yOrient) {
+         y--;
+         if (y < 0) {
+            y = 0;
+            yOrient = false;
+         }
+      } else {
+         y++;
+         if (y >= d.height - 20) {
+            y = d.height - 20;
+            yOrient = true;
+         }
+      }
+      int r = ran.nextInt(255);
+      int g = ran.nextInt(255);
+      int b = ran.nextInt(255);
+      gr.setColor(new Color(r, g, b));
+      gr.drawRect(x, y, 20, 20);
+   }
+
+   public void update(Graphics g) {
+      paint(g);
+   }
+   public void run(){
+      while(true){
+         repaint();
+         try{
+            Thread.sleep(10);
+         }catch(InterruptedException e){
+            e.printStackTrace();
+         }
+      }
+   }
+   public static void main(String[] args) {
+      ThreadRectDraw thread = new ThreadRectDraw("ThreadRectDraw");
+      Thread t1 = new Thread(thread);
+      t1.start();
+   }
+}
