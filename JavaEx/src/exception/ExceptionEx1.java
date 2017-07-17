dd4f800e77b063a2054824d762a83616d61f2060
kkyzzang016 <kkyzzang016@nate.com>
2017-07-17 10:54:38 +0900

    2017-07-17

---
+++ JavaEx/src/exception/ExceptionEx1.java
@@ -0,0 +1,26 @@
+package exception;
+
+public class ExceptionEx1 {
+	public static void main(String[] args) {
+		
+		try {
+			int[] array = new int[3];
+			array[0]=0;
+			array[1]=10;
+			array[2]=20;
+			
+			for(int i=0;i<array.length;i++) {
+				System.out.println("array["+i+"] = "+array[i]);
+			}
+		
+		}catch(ArrayIndexOutOfBoundsException e) {
+			System.out.println(e.toString());
+			System.out.println(e.getMessage());
+			e.printStackTrace();
+			return;
+			
+		}finally {
+			System.out.println("finally ½ÇÇà!!!!");
+		}
+	}
+}
---
+++ JavaEx/src/exception/ExceptionEx3.java
@@ -0,0 +1,28 @@
+package exception;
+
+public class ExceptionEx3 {
+
+	String str = "";
+	public void foo(int i) {
+		try {
+			if(i ==1) {
+				throw new Exception();
+			}
+			str+=" 1 ";
+		}
+		catch(Exception e) {
+			str += " 2 ";
+			return;
+		}
+		finally {
+			str += " 3 ";
+		}
+		str +=" 4 ";
+	}
+	public static void main(String[] args) {
+		ExceptionEx3 ex3 = new ExceptionEx3();
+		ex3.foo(0);
+		ex3.foo(1);
+		System.out.println(ex3.str);
+	}
+}
