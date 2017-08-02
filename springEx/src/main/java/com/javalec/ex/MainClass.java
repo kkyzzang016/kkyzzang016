38821bc92b1940f9560a4ecc34cd2d5d0dee84b9
kkyzzang016 <kkyzzang016@nate.com>
2017-08-02 17:42:26 +0900

    2017-08-02

---
+++ springEx/src/main/java/com/javalec/ex/MainClass.java
@@ -0,0 +1,15 @@
+package com.javalec.ex;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		Calculation cal = new Calculation();
+		cal.setFirstNum(10);
+		cal.setSecondNum(2);
+
+		cal.add();
+		cal.sub();
+		cal.multi();
+		cal.div();
+	}
+}
