88e4c3ed3051b2ff5f538093b9b288b86397eeb8
kkyzzang016 <kkyzzang016@nate.com>
2017-08-29 17:36:30 +0900

    2017-08-29

---
+++ JspEx/src/util/DBManager.java
@@ -0,0 +1,46 @@
+package util;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.sql.DataSource;
+
+public class DBManager {
+
+	public static Connection getConnection() {
+		Connection conn = null;
+		try {
+			Context initContext = new InitialContext();
+			Context envContext = (Context) initContext.lookup("java:/comp/env");
+			//jdbc/myoracle이란 이름을 객체로 찾아서 DataSource가 받는다.
+			DataSource ds = (DataSource)envContext.lookup("jdbc/mysql");
+			conn = ds.getConnection();
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+		return conn;
+	}
+	
+	//select를 수행한 후 리소스 해제를 위한 메소드
+	public static void close(Connection conn, Statement stmt, ResultSet rs) {
+		try {
+			rs.close();
+			stmt.close();
+			conn.close();
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void close(Connection conn, Statement stmt) {
+		try {
+			stmt.close();
+			conn.close();
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
